1cd92e9394a2c70c108433c5541908d704dbe212
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-15 11:12:55 -0800

    create avatar component

--- app/index.js
+++ app/index.js
@@ -9,7 +9,47 @@ var HelloWorld = React.createClass({
   }
 });
 
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
+  }
+})
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+})
+
+var ProfileName = React.createClass({
+  render: function (){
+    return(
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+})
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <Profilelink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 )
