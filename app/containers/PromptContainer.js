c5b03337753187ae8c54944f7bae58ca01862244
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-16 10:07:36 -0800

    write for routes and refactor prompt container

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,46 @@
+var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt')
+
+var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return (
+      username: ''
+    )
+  },
+  onUpdateUser: function (e) {
+    this.setState({
+      username: e.target.value
+    })
+  },
+  handleSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+
+    if(this.props.routeParams.playerOne) {
+      this.context.route.push({
+        pathname: 'battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo' + this.state.username)
+    }
+  },
+  render: function () {
+    return (
+      <Prompt
+        onSubmitUser={this.handleSubmitUser}/>
+    )
+  }
+});
+
+module.exports = PromptContainer;
