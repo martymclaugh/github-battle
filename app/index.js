509e6381b9e171a5aea7c1194bc66ffb3c41208a
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-16 09:29:26 -0800

    cl for debug

--- app/index.js
+++ app/index.js
@@ -1,55 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function () {
-    return (
-      <div> Hello World </div>
-    )
-  }
-});
-
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
-  }
-})
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <Link href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-})
-
-var ProfileName = React.createClass({
-  render: function (){
-    return(
-      <div>
-        {this.props.name}
-      </div>
-    )
-  }
-})
-
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <Profilelink username={this.props.user.username} />
-      </div>
-    )
-  }
-})
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  <Avatar user={} />,
   document.getElementById('app')
 )
--- containers/PromptContainer.js
+++ containers/PromptContainer.js
@@ -2,6 +2,7 @@ var React = require('react');
 
 var PromptContainer = React.createClass({
   render: function () {
+    console.log(this);
     return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
         <h1>SOME HEADER TEXT</h1>
