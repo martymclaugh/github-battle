dab38a0f0c08c218e1e1b3c31f19ce8ccab2cdf0
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-16 09:25:11 -0800

    create prompt container component

---
+++ containers/PromptContainer.js
@@ -0,0 +1,25 @@
+var React = require('react');
+
+var PromptContainer = React.createClass({
+  render: function () {
+    return (
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <h1>SOME HEADER TEXT</h1>
+        <div className="col-sm-12">
+          <form>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Github Username" />
+            </div>
+          </form>
+          <div className="form-group col-sm-4 col-sm-off">
+            <button className="btn btn-block btn-success" type="submit">
+              Continue
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = PromptContainer;
